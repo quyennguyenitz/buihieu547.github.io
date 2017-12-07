dda69836a492a359e74b5ca2bd58c23e1a2808c6
buihieu547 <33684549+buihieu547@users.noreply.github.com>
2017-12-07 14:24:55 +0700

    Update linked.js

--- assets/js/linked.js
+++ assets/js/linked.js
@@ -1,1 +1,13 @@
-
+IN.UI.Authorize().place();
+IN.Event.on(IN, 'auth', function() {
+  $.ajax({
+    url: 'https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=786u8yps549myw&redirect_uri=https%3A%2F%2Fbuihieu547.github.io%2F',
+    type: 'GET',
+    dataType: 'json',
+    crossDomain: true,
+    contentType: 'application/json; charset=utf-8',
+    success: function (data) {
+      console.log(data);
+    }
+  });
+});
